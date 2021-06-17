938cf84aa90f67a056efc3bcc3ba101798379317
AngelaSophia <ayugisophiaangela@gmail.com>
2021-06-17 10:50:13 +0300

    Sad

---
+++ python_test.py
@@ -0,0 +1,49 @@
+listx=[100,110,120,130,140,150]
+listy=[]
+z=[25,30,35,40,45,50,55]
+
+listy=[25,45,50,55,60,65]
+def divisible_by_three(number):
+    x=range(1,n)
+    
+
+def return_list():
+    y=[[1,2],[3,4],[5,6]],
+    x=[]
+    print(type(y))
+    
+
+def smallest(list):
+    for num in list:
+        print(sorted.num(2,4,5,7,8,1))
+
+def dublicate():
+      a=['a','b','a','e','d','b','c','e','f','g','h']
+      b=[] 
+      return a 
+
+def divisible_by_seven():
+    x=range(100,200)
+    for y in x:
+        if 7%==0
+        print(y)
+
+
+def greetStudent():
+    student1=[{"age":19,"name":"Eunice"}],
+    student2=[{"age":21,"name":"Agnes"}],
+    studen3=[{"age":18,"name":"Teresa"}],
+    student4=[{"age":22,"name":"Asha"}]
+    age=19
+    year_of_birth=2021-age
+    print(year_of_birth)
+   
+    print (student1(f"Hello {name} you were born in {year_of_birth}"))
+
+
+
+        
+
+
+
+
